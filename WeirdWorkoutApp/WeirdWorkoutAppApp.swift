419b48697ba20243916bf92c1ca98b31bd67a6b4
BoogieManN8 <tengizkiknadze8@gmail.com>
2024-02-19 15:22:23 +0400

    Initial Commit

---
+++ WeirdWorkoutApp/WeirdWorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeirdWorkoutAppApp.swift
+//  WeirdWorkoutApp
+//
+//  Created by MoonlitPrins8 on 19.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WeirdWorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
